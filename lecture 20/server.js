12c5afa378973a07a8c8c47fc6c85083a9825526
vasuaggarwal06 <vasuaggarwal2006@gmail.com>
2026-09-18 23:18:05 +0530

    daily upload 18-09-26

--- lecture 20/server.js
+++ lecture 20/server.js
@@ -1,3 +1,5 @@
 //create a random api key for your application. You can use a library like `crypto` in Node.js to generate a secure random API key. Here's an example of how to do it:
 
 const apiKey = crypto.randomBytes(32).toString("hex");
+const express = require("express");
+const app = express();
